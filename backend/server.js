256a0fa83b43959fd1ac6e194539b194540348ba
Turkay Tunc <turkaytunc00@gmail.com>
2020-10-16 15:04:29 +0300

    add helmet package for security

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
-const express = require('express');
-const app = express();
+const app = require('express')();
+const cors = require('cors');
+const helmet = require('helmet');
+
+app.use(cors());
+app.use(helmet());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(3001, () => {
   console.log('server started on port 3001');
